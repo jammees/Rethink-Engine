597bcf38f93c3097f82ad864f7570e5372a13a40
jammees <86011536+jammees@users.noreply.github.com>
2021-12-12 01:54:24 +0100

    remove

--- src/engine/README.lua
+++
@@ -1,8 +0,0 @@
---[[
-
-    You can change some engine behaviors in components/engineSettings
-
-    This 2D game engine is using Nature2D by jaipack17
-    Github repo: https://github.com/jaipack17/Nature2D
-
-]]
