f0a12d18ca9c3d019dfef3d16ff79528bea0da47
jammees <86011536+jammees@users.noreply.github.com>
2021-12-14 10:20:33 +0100

    Create init.lua

---
+++ src/engine/tools/inputs/init.lua
@@ -0,0 +1,19 @@
+--[[
+
+    Simply used to fetch the input modules more easily
+
+]]
+
+return {
+    current = require(script.currentInput),
+
+    --[[
+
+        [Not Implemented Yet]
+        
+        gamepad = require(script.gamepad),
+        keybaord = require(script.keybaord),
+        mouse = require(script.mouse),
+        touch = require(script.touch),
+    ]]
+}
