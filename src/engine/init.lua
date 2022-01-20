af31cac2db3c442ca336af1a8badda469eedd93e
jammees <86011536+jammees@users.noreply.github.com>
2022-01-20 20:38:05 +0100

    Cleanup

--- src/engine/init.lua
+++ src/engine/init.lua
@@ -1,119 +1,65 @@
 --[[
 
-    RethinkEngine                                      @JamRBX
+    RethinkEngine                                  
+    @JamRBX (twitter)
 
-    Version: 0.5.0 - ALPHA
-    Description: A simple yet useful 2D game engine for roblox
+    Credit goes to:
+    @AstrealDev (devforum)
+    @jaipack17 (devforum)
 
-    Recommened to use a ui library for creating uis for your game, such as Roact or Fusion
-
-]] 
+]]
 
 -- get tools
-local started = false
-
-local components = script:WaitForChild("components")
-
-local tools = script:WaitForChild("tools")
-local nature2D = require(tools.physics)
-
--- get player gui
-local player = game:GetService("Players").LocalPlayer
-local playerGui = player:WaitForChild("PlayerGui")
-
--- SET UP NATURE 2D
---------------------------------------------------------------------------------------------
--- set up game frame
-local gameFrame = nil
-local renderFrame = nil
-local canvas = nil
-local layer = nil
-local hud = nil
+local isRunning = false
+local requiredTools = {}
+local components = script:WaitForChild("Components")
+local tools = script:WaitForChild("Tools")
 
 --// modules
 local engine = nil
-local rigid = nil
-local globals = require(script.globals)
+local setup = require(components.Setup)
+local readOnly = require(components.ReadOnly)
+local settings = require(components.Settings)
+local roactive = require(components.Roactive)
+local wrapper = require(components.Wrapper)
+local exposedStates = roactive.State(setup()) -- setups ui and exposes it
+local nature2D = require(tools.Physics)
 
-if not playerGui:FindFirstChild("gameFrame") then
-    gameFrame = Instance.new("ScreenGui")
-    gameFrame.Name = "gameFrame"
-    gameFrame.IgnoreGuiInset = true
-    gameFrame.ResetOnSpawn = false
-    gameFrame.Parent = playerGui
+if not isRunning then
+	isRunning = true
+	settings()
+	-- initiate Nature2D
+	engine = nature2D.init(exposedStates:Get().gameFrame)
+	engine.canvas.frame = exposedStates:Get().canvas
 
-    -- set up a scrolling frame for the whole level
-    renderFrame = Instance.new("ScrollingFrame")
-    renderFrame.Name = "renderFrame"
-    renderFrame.AnchorPoint = Vector2.new(0.5, 0.5)
-    renderFrame.Position = UDim2.fromScale(0.5, 0.5)
-    renderFrame.Size = UDim2.fromScale(1, 1)
-    renderFrame.CanvasSize = UDim2.new(0, 0, 0, 0) -- [!] Not to self: Remove this after testing level loading!
-    renderFrame.Transparency = 1
-    renderFrame.BorderSizePixel = 0
-    renderFrame.Parent = gameFrame
+	wrapper.giveData({
+		Engine = engine,
+		Ui = exposedStates:Get(),
+	})
 
-    -- set up canvas for Nature2D
-    canvas = Instance.new("Frame")
-    canvas.Name = "canvas"
-    canvas.AnchorPoint = Vector2.new(0.5, 0.5)
-    canvas.Position = UDim2.fromScale(0.5, 0.5)
-    canvas.Size = UDim2.fromScale(1, 1)
-    canvas.Transparency = 1
-    canvas.Parent = renderFrame
-
-    -- setup a frame to hold ui elements
-    hud = Instance.new("Frame")
-    hud.Name = "ui"
-    hud.AnchorPoint = Vector2.new(0.5, 0.5)
-    hud.Position = UDim2.fromScale(0.5, 0.5)
-    hud.Size = UDim2.fromScale(1, 1)
-    hud.Transparency = 1
-    hud.Parent = renderFrame
-
-    -- set up a frame for holding the parts that don't interact with Nature2D
-    layer = Instance.new("Frame")
-    layer.Name = "layer"
-    layer.AnchorPoint = Vector2.new(0.5, 0.5)
-    layer.Position = UDim2.fromScale(0.5, 0.5)
-    layer.Size = UDim2.fromScale(1, 1)
-    layer.Transparency = 1
-    layer.Parent = renderFrame
+	--[[
+	for _, tool in ipairs(tools:GetChildren()) do
+		local required = require(tool)
+		pcall(function()
+			required._setReq(exposedStates:Get(), engine)
+		end)
+		requiredTools[tool.Name] = required
+	end
+	-]]
 end
 
-if not started then
-    started = true
+return readOnly({
+	Physics = engine,
+	--Rigid = require(tools.Rigid),
+	Scene = require(tools.Scene),
+	Inputs = require(tools.Inputs),
+	Outline = require(tools.Outline),
+	Template = require(tools.Template),
+	Roactive = require(components.Roactive),
 
-    -- initiate it
-    engine = nature2D.init(gameFrame)
-    engine.canvas.frame = canvas
-    --------------------------------------------------------------------------------------------
+	Ui = exposedStates:Get(),
 
-    -- run the engineSettings
-    require(components.engineSettings)
-    
-    -- setup some tools
-    rigid = require(tools.rigid)
-    rigid:_setEngine(engine, canvas)
-
-    -- setup some globals
-    globals.set("window", gameFrame)
-    globals.set("render", renderFrame)
-    globals.set{"canvas", canvas}
-    globals.set("layer", layer)
-    globals.set("hud", hud)
-    globals.set("physics", engine)
-end
-
-return {
-    physics = engine,
-    rigid = rigid,
-    scene = require(tools.scene),
-    inputs = require(tools.inputs),
-
-    gameWindow = gameFrame,
-    gameRender = renderFrame,
-    canvas = canvas,
-    layer = layer,
-    hud = hud,
-}
+	isRunning = function()
+		return isRunning
+	end,
+})
