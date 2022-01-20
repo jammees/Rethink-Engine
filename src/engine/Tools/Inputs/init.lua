d11600b247f5136945f4068c70a3a08945fa8737
jammees <86011536+jammees@users.noreply.github.com>
2022-01-20 23:30:26 +0100

    Updated init

--- src/engine/Tools/Inputs/init.lua
+++ src/engine/Tools/Inputs/init.lua
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 -- Used to get input modules much more easier
 
 return {
@@ -13,3 +14,20 @@ return {
         Touch = require(script.Touch),
     ]]
 }
+=======
+-- Used to get input modules much more easier
+
+return {
+	Current = require(script.Current),
+	Gamepad = require(script.Gamepad),
+
+	--[[
+
+        [Not Implemented Yet]
+        
+        Keybaord = require(script.Keybaord),
+        Mouse = require(script.Mouse),
+        Touch = require(script.Touch),
+    ]]
+}
+>>>>>>> Stashed changes
