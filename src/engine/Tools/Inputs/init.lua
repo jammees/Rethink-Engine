3dde3f92e569874a1e5b3aad0eeb28e6eaa3763e
James <86011536+jammees@users.noreply.github.com>
2022-01-20 22:28:14 +0100

    Readded init file

---
+++ src/engine/Tools/Inputs/init.lua
@@ -0,0 +1,15 @@
+-- Used to get input modules much more easier
+
+return {
+	Current = require(script.Current),
+	Gamepad = require(script.Gamepad),
+
+	--[[
+
+        [Not Implemented Yet]
+        
+        Keybaord = require(script.Keybaord),
+        Mouse = require(script.Mouse),
+        Touch = require(script.Touch),
+    ]]
+}
