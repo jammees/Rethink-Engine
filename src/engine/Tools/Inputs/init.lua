b5847b277702da42bc7f70f68715fd51519443d8
jammees <86011536+jammees@users.noreply.github.com>
2022-01-20 23:37:42 +0100

    Fixed commit problem

--- src/engine/Tools/Inputs/init.lua
+++ src/engine/Tools/Inputs/init.lua
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 -- Used to get input modules much more easier
 
 return {
@@ -14,20 +13,3 @@ return {
         Touch = require(script.Touch),
     ]]
 }
-=======
--- Used to get input modules much more easier
-
-return {
-	Current = require(script.Current),
-	Gamepad = require(script.Gamepad),
-
-	--[[
-
-        [Not Implemented Yet]
-        
-        Keybaord = require(script.Keybaord),
-        Mouse = require(script.Mouse),
-        Touch = require(script.Touch),
-    ]]
-}
->>>>>>> Stashed changes
