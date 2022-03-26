9ded0a4d61f57af7410302a1f96d4c109a673ba0
jammees <86011536+jammees@users.noreply.github.com>
2022-03-26 17:34:56 +0100

    Updated for debug

--- src/test/New/Tests/Test_Scene.client.lua
+++ src/test/New/Tests/Test_Scene.client.lua
@@ -1,7 +1,7 @@
 local rethinkEngine = require(game:GetService("ReplicatedStorage").RethinkEngine)
 local scene = rethinkEngine.Scene
 
-scene:Load({
+local testScene = {
 	Data = {
 		Name = "myTestMap",
 	},
@@ -79,4 +79,79 @@ scene:Load({
 			},
 		},
 	},
-})
+}
+
+local sceneData = {
+	Structure = {
+		Layers = {
+			_Config = {
+				Type = "Layer",
+				UniversalProperty = {
+					BackgroundColor3 = Color3.fromRGB(12, 206, 77),
+				},
+			},
+
+			["My Epic Group:-1"] = {
+				_UniversalProperty = {
+					BackgroundColor3 = Color3.fromRGB(207, 45, 45),
+				},
+				MyRedObject = {
+					BackgroundColor3 = Color3.fromRGB(27, 39, 209),
+					Position = UDim2.fromScale(0.5, 0.5),
+					AnchorPoint = Vector2.new(0.5, 0.5),
+					Size = UDim2.fromOffset(75, 75),
+				},
+			},
+			["My another epic group:2"] = {
+				MyRedButOverridedGreenObject = {
+					BackgroundColor3 = Color3.fromRGB(209, 27, 133),
+					Position = UDim2.fromScale(0.5, 0.4),
+					AnchorPoint = Vector2.new(0.5, 0.5),
+					Size = UDim2.fromOffset(75, 75),
+				},
+			},
+		},
+		Rigidbodies = {
+			_Config = {
+				Type = "Rigidbody",
+				UniversalProperty = {
+					BackgroundColor3 = Color3.fromRGB(9, 113, 161),
+					Anchored = true,
+				},
+			},
+
+			["A rigidbody group"] = {
+				Ground = {
+					BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+					Position = UDim2.fromScale(0.5, 1),
+					AnchorPoint = Vector2.new(0.5, 1),
+					Size = UDim2.new(1, 0, 0, 45),
+				},
+			},
+			["Coins"] = {
+				_UniversalProperty = {
+					Tag = "coin",
+					ImageColor3 = Color3.fromRGB(255, 0, 0),
+				},
+				Coin = {
+					Class = "ImageLabel",
+					Size = UDim2.fromOffset(50, 50),
+					Position = UDim2.fromScale(0.5, 0.25),
+					Image = "rbxassetid://7058514483",
+					AnchorPoint = UDim2.fromScale(0.5, 0.5),
+					BackgroundTransparency = 1,
+				},
+				Coin2 = {
+					Class = "ImageLabel",
+					Size = UDim2.fromOffset(50, 50),
+					Position = UDim2.fromScale(0.25, 0.25),
+					Image = "rbxassetid://7058514483",
+					AnchorPoint = UDim2.fromScale(0.5, 0.5),
+					BackgroundTransparency = 1,
+				},
+			},
+		},
+	},
+}
+
+scene:Load(sceneData)
--- src/test/New/Tests/Test_Start_Simulation.client.lua
+++ src/test/New/Tests/Test_Start_Simulation.client.lua
@@ -1,11 +1,21 @@
+local RunService = game:GetService("RunService")
+
 local rethinkEngine = require(game:GetService("ReplicatedStorage").RethinkEngine)
 local sceneManager = rethinkEngine.Scene
 local engine = rethinkEngine.Physics
+local camera = rethinkEngine.Camera
+
+local renderStepped = nil
 
 game:GetService("UserInputService").InputBegan:Connect(function(key)
 	if key.KeyCode == Enum.KeyCode.S then
 		engine:Start()
+
+		--renderStepped = RunService.RenderStepped:Connect(function(dt)
+		--	camera:Render(dt)
+		--end)
 	elseif key.KeyCode == Enum.KeyCode.D then
 		sceneManager:Flush()
+		renderStepped:Disconnect()
 	end
 end)
