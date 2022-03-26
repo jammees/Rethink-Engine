abc5d9b13779cb40777351c7abb12b505679aeeb
jammees <86011536+jammees@users.noreply.github.com>
2022-03-26 17:35:11 +0100

    Updated Template

--- src/engine/Tools/Template.lua
+++ src/engine/Tools/Template.lua
@@ -16,24 +16,24 @@
 local template = {}
 template.__index = template
 
-function template.new(element)
+function template.New(element)
 	element.Parent = nil
 	return setmetatable({
 		ui = element,
 	}, template)
 end
 
-function template:fetch()
+function template:Fetch()
 	return self.ui:Clone()
 end
 
-function template:update(element)
+function template:Update(element)
 	if element:IsA("GuiBase2d") and self.ui ~= element then
 		self.ui = element
 	end
 end
 
-function template:destroy()
+function template:Destroy()
 	self.ui:Destroy()
 end
 
