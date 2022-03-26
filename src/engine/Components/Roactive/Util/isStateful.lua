1dc52b400ac98b6b4c922b54875f620d9f0c79a9
jammees <86011536+jammees@users.noreply.github.com>
2022-03-26 17:30:18 +0100

    Removed Roactive

--- src/engine/Components/Roactive/Util/isStateful.lua
+++
@@ -1,9 +0,0 @@
-local StatefulObjectTypes = {
-  'State',
-  'Delay',
-  'Stopwatch',
-}
-
-return function(object: any)
-  return typeof(object) == 'table' and table.find(StatefulObjectTypes, object.type)
-end
