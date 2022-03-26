d383025913e265d5ef6222a31aa22bcd0fd93f70
jammees <86011536+jammees@users.noreply.github.com>
2022-03-26 17:32:14 +0100

    Updated Wrapper

--- src/engine/Components/Wrapper.lua
+++ src/engine/Components/Wrapper.lua
@@ -7,26 +7,27 @@
 
 ]]
 
-local saveData = nil
+local saveData = {}
 
 local wrapper = {}
 wrapper.__index = wrapper
 
-function wrapper.giveData(array: {})
+function wrapper.GiveData(array: { [string]: any })
 	saveData = array
 end
 
-function wrapper.wrap()
+function wrapper.Wrap()
 	return setmetatable({
 		-- modules
 		Engine = saveData.Engine,
 
 		-- ui
-		GameFrame = saveData.Ui.gameFrame,
-		RenderFrame = saveData.Ui.renderFrame,
-		Canvas = saveData.Ui.canvas,
-		Layer = saveData.Ui.layer,
-		Hud = saveData.Ui.hud,
+		GameFrame = saveData.Ui.GameFrame,
+		RenderFrame = saveData.Ui.RenderFrame,
+		Canvas = saveData.Ui.Canvas,
+		Layer = saveData.Ui.Layer,
+		Ui = saveData.Ui.Ui,
+		CameraCont = saveData.Ui.CameraCont,
 	}, wrapper)
 end
 
