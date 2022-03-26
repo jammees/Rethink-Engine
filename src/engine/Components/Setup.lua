d21ed5c6872d8d2ce99044071bd8c0d9e3c344f4
jammees <86011536+jammees@users.noreply.github.com>
2022-03-26 17:31:37 +0100

    Updated Setup

--- src/engine/Components/Setup.lua
+++ src/engine/Components/Setup.lua
@@ -3,9 +3,11 @@ local playerGui = player:WaitForChild("PlayerGui")
 
 local gameFrame = nil
 local renderFrame = nil
+local ui = nil
+
+local camera = nil
 local canvas = nil
 local layer = nil
-local hud = nil
 
 local function checkIfAlreadyExists(frame)
 	if gameFrame and playerGui:WaitForChild(frame) then
@@ -17,57 +19,65 @@ end
 local function setup()
 	if not checkIfAlreadyExists(gameFrame) then
 		gameFrame = Instance.new("ScreenGui")
-		gameFrame.Name = "gameFrame"
+		gameFrame.Name = "GameFrame"
 		gameFrame.IgnoreGuiInset = true
 		gameFrame.ResetOnSpawn = false
 		gameFrame.Parent = playerGui
 
 		-- set up a scrolling frame for the whole level
-		renderFrame = Instance.new("ScrollingFrame")
-		renderFrame.Name = "renderFrame"
+		renderFrame = Instance.new("Frame")
+		renderFrame.Name = "RenderFrame"
 		renderFrame.AnchorPoint = Vector2.new(0.5, 0.5)
 		renderFrame.Position = UDim2.fromScale(0.5, 0.5)
 		renderFrame.Size = UDim2.fromScale(1, 1)
-		renderFrame.CanvasSize = UDim2.new(0, 0, 0, 0) -- [!] Not to self: Remove this after testing level loading!
 		renderFrame.Transparency = 1
 		renderFrame.BorderSizePixel = 0
 		renderFrame.Parent = gameFrame
 
+		-- for camera to move the uis
+		camera = Instance.new("Frame")
+		camera.Name = "CameraContainer"
+		camera.AnchorPoint = Vector2.new(0.5, 0.5)
+		camera.Position = UDim2.fromScale(0.5, 0.5)
+		camera.Size = UDim2.fromScale(1, 1)
+		camera.Transparency = 1
+		camera.Parent = renderFrame
+
 		-- set up canvas for Nature2D
 		canvas = Instance.new("Frame")
-		canvas.Name = "canvas"
+		canvas.Name = "Canvas"
 		canvas.AnchorPoint = Vector2.new(0.5, 0.5)
 		canvas.Position = UDim2.fromScale(0.5, 0.5)
 		canvas.Size = UDim2.fromScale(1, 1)
 		canvas.Transparency = 1
-		canvas.Parent = renderFrame
-
-		-- setup a frame to hold ui elements
-		hud = Instance.new("Frame")
-		hud.Name = "ui"
-		hud.AnchorPoint = Vector2.new(0.5, 0.5)
-		hud.Position = UDim2.fromScale(0.5, 0.5)
-		hud.Size = UDim2.fromScale(1, 1)
-		hud.Transparency = 1
-		hud.Parent = renderFrame
+		canvas.Parent = camera
 
 		-- set up a frame for holding the parts that don't interact with Nature2D
 		layer = Instance.new("Frame")
-		layer.Name = "layer"
+		layer.Name = "Layer"
 		layer.AnchorPoint = Vector2.new(0.5, 0.5)
 		layer.Position = UDim2.fromScale(0.5, 0.5)
 		layer.Size = UDim2.fromScale(1, 1)
 		layer.Transparency = 1
-		layer.Parent = renderFrame
-	else
+		layer.Parent = camera
+
+		-- setup a frame to hold ui elements
+		ui = Instance.new("Frame")
+		ui.Name = "Ui"
+		ui.AnchorPoint = Vector2.new(0.5, 0.5)
+		ui.Position = UDim2.fromScale(0.5, 0.5)
+		ui.Size = UDim2.fromScale(1, 1)
+		ui.Transparency = 1
+		ui.Parent = renderFrame
 	end
 
 	return {
-		gameFrame = gameFrame,
-		renderFrame = renderFrame,
-		canvas = canvas,
-		layer = layer,
-		hud = hud,
+		GameFrame = gameFrame,
+		RenderFrame = renderFrame,
+		Canvas = canvas,
+		Layer = layer,
+		Ui = ui,
+		CameraCont = camera,
 	}
 end
 
