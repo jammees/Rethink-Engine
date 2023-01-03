db0f8f77c2c956ada693caa8583c98a546dcd2e7
James <86011536+jammees@users.noreply.github.com>
2023-01-03 21:14:29 +0100

    Improved initiating settings controllers

--- src/Components/Bootstrap/EngineSettings/init.lua
+++ src/Components/Bootstrap/EngineSettings/init.lua
@@ -1,20 +1,21 @@
-local pacakge = script.Parent.Parent.Parent
-local tools = pacakge:WaitForChild("Tools")
-
-local Template = require(tools.Utility.Template)
-local Settings = require(script.Settings)
-
-local procedures = script.Procedures
+local package = script:FindFirstAncestor("Components").Parent
+local Settings = require(package.Settings)
 
 local SettingsController = {}
 
 function SettingsController.Run()
 	for category, values in pairs(Settings) do
-		if typeof(procedures[category]) == "Instance" and procedures[category]:IsA("ModuleScript") then
-			for procName, procCallback in pairs(require(procedures[category])) do
+		--if table.find(IGNORED_SETTINGS_REGISTRIES, category) then
+		--	continue
+		--end
+
+		local settingController = script:FindFirstChild(category)--script[category]
+
+		if typeof(settingController) == "Instance" and settingController:IsA("ModuleScript") then
+			for procName, procCallback in pairs(require(settingController)) do
 				if values[procName] ~= nil then
 					task.spawn(function()
-						procCallback(values[procName], Template)
+						procCallback(values[procName])
 					end)
 				end
 			end
