eff3c868d836690a51125447a16dbad9555fcbb4
James <86011536+jammees@users.noreply.github.com>
2023-01-03 21:46:35 +0100

    Utilize TaskDistributor
    Compiling objects in the thousands now is a smoother experience by dividing up the work with TaskDistributor

--- src/Tools/Core/Scene/Compiler.lua
+++ src/Tools/Core/Scene/Compiler.lua
@@ -1,97 +1,197 @@
-local StarterPack = game:GetService("StarterPack")
-local protocols = script.Parent.Protocols
+type SceneConfig = {
+	Name: string?,
+	CompileMode: string?,
+}
+
+type AttachedSymbolsHolder = {
+	Event: () -> nil?
+}
+
+type ChunkObject = {
+	ObjectIdentifier: string?,
+	ObjectData: { any },
+	GroupData: { any },
+	SavedProperties: { any },
+	ObjectType: string,
+	SymbolsAttached: AttachedSymbolsHolder,
+}
+
+type PackedSymbol = {
+	Symbol: {
+		Type: string,
+		Name: string,
+		Data: { any }?
+	},
+	Value: any
+}
+
+local ALIAS_OBJECTS_NAMES = {
+	Layer = "UiBase",
+	Static = "UiBase",
+	Dynamic = "Rigidbody",
+
+	-- Base 
+	Rigidbody = "Rigidbody",
+	UiBase = "UiBase",
+}
+
 local package = script.Parent.Parent.Parent.Parent
+local objects = script.Parent.Objects
 local components = package.Components
 
---local Wrapper = require(components.Wrapper)
-local Util = require(components.Util)
-local Types = require(components.Debug.Types)
-local Promise = require(components.Lib.Promise)
+local Promise = require(components.Library.Promise)
 local Symbols = require(script.Parent.Symbols)
-local Template = require(package.Tools.Utility.Template)
+local TaskDistributor = require(components.Library.TaskDistributor).new()
+local Settings = require(package.Settings)
+
+local CompilerObjects = {
+	Rigidbody = require(objects.Rigidbody),
+	UiBase = require(objects.UiBase),
+}
 
 local Compiler = {}
+Compiler.CompilerDistributor = TaskDistributor
 
-local standardProtocols = protocols.Standard
-local savedProperties = {}
-
-local function ValidateProtocol(object: ModuleScript, fallbackName: string): ModuleScript
-	if typeof(object) == "Instance" and object:IsA("ModuleScript") then
-		return require(object)
-	else
-		return require(standardProtocols[fallbackName])
+local function PackSymbol(symbol, value)
+	if typeof(symbol) == "nil" then
+		return nil
 	end
+
+	return {
+		Symbol = symbol,
+		Value = value
+	}
 end
 
-local function CompileData(protocolHandlers: Folder, objectArray: { [number]: any }, protocolId: string)
-	local compiledObjects = {}
+local function IsSymbol(tableIndex: any): boolean
+	if typeof(tableIndex) == "table" and tableIndex.Name ~= nil then
+		return true
+	end
 
-	for _, compilerData in ipairs(objectArray) do
-		for groupIndex, groupData in pairs(compilerData) do
-			-- Try and optimize/speedup the compiling process with threads?
+	return false
+end
 
-			--[[
-				Benchmarking results:
+local function BACKUP_MergeSceneData(sceneData: { [ number ]: any }): { [number]: ChunkObject }
+	local mappedData = {}
+	local savedProperties = {}
+	local objectType = nil
 
-				Without using task.spawn
+	for _, sceneCategory in pairs(sceneData) do
+		-- Check if we can find a table with a [property] symbol attacjed
+		-- As well as find the type of the category
+		savedProperties = select(2, Symbols.FindSymbol(sceneCategory, "Property"))
+		objectType = select(2, Symbols.FindSymbol(sceneCategory, "Type"))
 
-				(50/50 cycles // 100.0 %):
-				Avarage: 0.499642 μs
-				Maximum: 0.765146 μs
-				Minimum: 0.255104 μs
-				Elapsed: 24.982083 μs
+		for groupKey, groupData in pairs(sceneCategory) do
+			if typeof(groupData) ~= "table" or typeof(groupKey) == "table" and Symbols.Types[groupKey.Name] ~= nil then
+				continue
+			end
 
-				With using task.spawn
-
-				(50/50 cycles // 100.0 %):
-				Avarage: 0.477752 μs
-				Maximum: 0.779514 μs
-				Minimum: 0.264649 μs
-				Elapsed: 23.887622 μs
-			]]
-
-			Util.Compiler.IsValidSymbol(compilerData, Symbols.Property.Name, function(result)
-				savedProperties = result
-			end)
-
-			if typeof(groupData) == "table" and typeof(groupIndex) ~= "table" then
-				for layerIndex, layerData in pairs(groupData) do
-					if typeof(layerData) == "table" and typeof(layerIndex) ~= "table" then
-						-- find the compile mode
-						local handler = ValidateProtocol(protocolHandlers[protocolId], protocolId)
-
-						local lData = { Index = layerIndex, Data = layerData }
-						local gData = { Index = groupIndex, Data = groupData }
-						local parent =
-							Template.FetchGlobal("__Rethink_Ui")[protocolId == "Layer" and "Layer" or "Canvas"]
-
-						-- create the obhect
-						local object = handler(lData, gData, savedProperties, parent)
-
-						-- save it
-						table.insert(compiledObjects, object)
-					end
+			for objectKey, objectData in pairs(groupData) do
+				if typeof(objectKey) == "table" and Symbols.Types[objectKey.Name] ~= nil then
+					continue
 				end
+
+				-- Handle symbols
+				local SymbolsAttached: AttachedSymbolsHolder = {}
+
+				-- Loop over the attachable symbols
+				-- This is here to automate this process, so every time I would add a new attachable symbol
+				-- I would not have to go here and edit this
+				for symbolName, _ in pairs(Symbols.AttachableSymbols) do
+					SymbolsAttached[symbolName] = PackSymbol(Symbols.FindSymbol(objectData, "Event"))
+				end
+
+				-- Save every necessary informations into a single table
+				-- So TaskDistributor has access to every piece of data that is required to construct an object
+				table.insert(mappedData, {
+					ObjectIdentifier = objectKey,
+					ObjectData = objectData,
+					GroupData = groupData,
+					SavedProperties = savedProperties,
+					ObjectType = objectType,
+					SymbolsAttached = SymbolsAttached
+				})
 			end
 		end
 	end
 
-	return compiledObjects
+	return mappedData
 end
 
-function Compiler.Compile(sceneConfig: Types.SceneConfig, sceneData: { any })
-	--table.clear(savedProperties)
+local function MergeSceneData(sceneData: { [ number ]: any }): { [number]: ChunkObject }
+	local mappedData = {}
+	local savedProperties = {}
+	local objectType = nil
 
+	for _, sceneCategory in pairs(sceneData) do
+		-- Check if we can find a table with a [property] symbol attacjed
+		-- As well as find the type of the category
+		savedProperties = select(2, Symbols.FindSymbol(sceneCategory, "Property"))
+		objectType = select(2, Symbols.FindSymbol(sceneCategory, "Type"))
+
+		for groupKey, groupData in pairs(sceneCategory) do
+			if IsSymbol(groupKey) then
+				continue
+			end
+
+			for objectKey, objectData in pairs(groupData) do
+				if IsSymbol(objectKey) then
+					continue
+				end
+
+				-- Handle symbols
+				local SymbolsAttached: AttachedSymbolsHolder = {}
+
+				-- Loop over the attachable symbols
+				-- This is here to automate this process, so every time I would add a new attachable symbol
+				-- I would not have to go here and edit this
+				for symbolName, _ in pairs(Symbols.AttachableSymbols) do
+					SymbolsAttached[symbolName] = PackSymbol(Symbols.FindSymbol(objectData, "Event"))
+				end
+
+				-- Save every necessary informations into a single table
+				-- So TaskDistributor has access to every piece of data that is required to construct an object
+				table.insert(mappedData, {
+					ObjectIdentifier = objectKey,
+					ObjectData = objectData,
+					GroupData = groupData,
+					SavedProperties = savedProperties,
+					ObjectType = objectType,
+					SymbolsAttached = SymbolsAttached
+				})
+			end
+		end
+	end
+
+	return mappedData
+end
+
+function Compiler.Compile(sceneData: { any }): { [number]: Instance }
+	local compiledObjects = {}
+
+	-- New and improved compiling method
+	-- In this method TaskDistributor is being used, that is a simple utility module
+	-- that is great for processing large amounts of data by utilizing Promises
+	-- this is kind of like using task.synchronize I think
 	return Promise.new(function(resolve)
-		local protocolHandlers = protocols:FindFirstChild(sceneConfig.CompileMode or "Standard")
+		TaskDistributor:Distribute(TaskDistributor.GenerateChunk(MergeSceneData(sceneData), Settings.CompilerChunkSize), function(object: ChunkObject)
+			-- Compile the object			
+			local compiledObject = CompilerObjects[ALIAS_OBJECTS_NAMES[object.ObjectType]]({ 
+				Index = object.ObjectIdentifier,
+				Data = object.ObjectData,
+			}, object.GroupData, object.SavedProperties)
 
-		savedProperties = {}
+			-- Try to attach symbols defined in the `object.SymbolsAttached` table
+			-- This needs to happen in this time, because when these actually get groupped together,
+			-- the object does not exist yet
+			Symbols.AttachToInstance(compiledObject, object.SymbolsAttached)
 
-		resolve(
-			CompileData(protocolHandlers, Util.SearchForType(sceneData, "Layer"), "Layer"),
-			CompileData(protocolHandlers, Util.SearchForType(sceneData, "Rigidbody"), "Rigidbody")
-		)
+			table.insert(compiledObjects, compiledObject)
+		end):await()
+
+		resolve(compiledObjects)
 	end):catch(warn)
 end
 
-return Compiler.Compile
+return Compiler
