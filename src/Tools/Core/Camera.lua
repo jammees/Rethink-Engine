be3722b44d1518983316b30bda187657cbbd0ad4
James <86011536+jammees@users.noreply.github.com>
2023-01-03 21:47:54 +0100

    Update API
    Attempted to fix rigidbodies freezing in the air. Still not working. In further inDev releases I'll try to figure out something to make it kinda work.

--- src/Tools/Core/Camera.lua
+++ src/Tools/Core/Camera.lua
@@ -1,208 +1,90 @@
-type TweenProperty = {
-	Duration: number,
-	Style: Enum.EasingStyle,
-	Direction: Enum.EasingDirection,
+-- Add a "OriginalPosition" property to the ObjectData table
+type ObjectData = {
+    Object: GuiBase2d | { any },
+    IsRigidbody: boolean,
+    OriginalPosition: UDim2
 }
 
-local TweenService = game:GetService("TweenService")
+local Scene = require(script.Parent.Scene)
 
-local zoomTween: Tween = nil
-local package = script.Parent.Parent.Parent
-local components = package.Components
+local prePosition = Vector2.new(0, 0)
 
-local Util = require(components.Util)
-local Strings = require(components.Debug.Strings)
--- local Collision = require(script.Parent.Parent.Environment.Collision)
-local Wrapper = require(components.Wrapper)
-
-local Camera = Wrapper.Wrap()
+local Camera = {}
 
 Camera.Position = Vector2.new(0, 0)
-Camera.PrePosition = Vector2.new(0, 0)
 Camera.Objects = {}
 
-local viewScale = Camera.ViewScale
--- local physics = Camera.physics
+-- Store the original position of the object when it is attached to the camera
+function Camera.Attach(object: GuiBase2d | {any}) 
+    local isRigidbody = Scene.IsRigidbody(object)
+    table.insert(Camera.Objects, {
+        Object = object,
+        IsRigidbody = isRigidbody,
+        OriginalPosition = isRigidbody and object:GetFrame().Position or object.Position
+    })
 
--- local connectionCache = {}
-
---[[ function Camera:DampenBounce()
-	local rigidbodies = physics:GetBodies()
-
-	for _, connection in ipairs(connectionCache) do
-		connection:Disconnect()
-	end
-	table.clear(connectionCache)
-
-	for _, body in ipairs(rigidbodies) do
-		if body.anchored then
-			connectionCache[#connectionCache + 1] = body.Touched:Connect(function(id)
-				local touchedBody = physics:GetBodyById(id)
-				if not touchedBody.anchored then
-					--warn(touchedBody:AverageVelocity().Y)
-					if touchedBody:AverageVelocity().Y > 1 then
-						touchedBody:Anchor()
-						local touchedFrame = touchedBody:GetFrame()
-						local _, depth = Collision.solidCollision(touchedFrame, body:GetFrame())
-						local X = touchedFrame.Position.X.Offset
-						local Y = touchedFrame.Position.Y.Offset + depth.Y
-
-						touchedBody:Unanchor()
-						touchedBody:SetPosition(X, Y)
-					end
-				end
-			end)
-		end
-	end
-end ]]
-
-local posbuffer = {}
-
-function Camera:Render()
-	-- if self.PrePosition ~= self.Position and #self.Objects > 0 then --> save some resources, hopefully :)
-	-- 	self.PrePosition = self.Position
-
-	-- 	for _, objectData in ipairs(self.Objects) do
-	-- 		local object: Instance | { any } = objectData.Object
-	-- 		local initPos: UDim2 = objectData.Position
-
-	-- 		local X = -self.Position.X - initPos.X.Offset
-	-- 		local Y = -self.Position.Y - initPos.Y.Offset
-
-	-- 		local deltaPosition = Camera.PrePosition - Camera.Position
-
-	-- 		-- if typeof(object) == "Instance" then
-	-- 		-- 	object.Position = UDim2.fromOffset(X, Y)
-	-- 		-- end
-
-	-- 		if object.id ~= nil then
-	-- 			local objInstance: Frame = object:GetFrame()
-	-- 			local objPosition: UDim2 = objInstance.Position
-	-- 			object:SetPosition(objPosition.X.Offset + deltaPosition.X, objPosition.Y.Offset + deltaPosition.Y)
-	-- 		end
-	-- 		-- if object.anchored then
-	-- 		-- 	object:SetPosition(X, Y)
-	-- 		-- else
-	-- 		-- 	return -- for now Camera won't support moving rigidbodies
-	-- 		-- 	-- object:SetPosition(X, Y)
-	-- 		-- 	-- object:ApplyForce(X / 2, Y / 2) <-- won't work pretty sure
-	-- 		-- end
-	-- 	end
-	-- end
-
-	if self.PrePosition ~= self.Position and #self.Objects > 0 then
-		self.PrePosition = self.Position --> stop updating
-
-		for _, objectData in ipairs(self.Objects) do
-			local object = objectData.Object
-			local initalPosition: UDim2 = objectData.Position
-
-			local X = -self.Position.X - initalPosition.X.Offset
-			local Y = -self.Position.Y - initalPosition.Y.Offset
-
-			local buffer = posbuffer[type(object) == "table" and object:GetFrame() or object]
-
-			local delta = Vector2.new(buffer.X - X, buffer.Y - Y)
-
-			-- somehow figure it out how to reference this code or get the data
-			-- to send it to the physics engine
-			-- so the body will move now instead of it getting freezed
-			if object.id then
-				--object:SetPosition
-				--rigidbuffer[object:GetFrame()] = delta
-
-				-- do something blah blah
-			else
-				object.Position += UDim2.fromOffset(delta.X, delta.Y)
-			end
-		end
-	end
+    warn(Camera.Objects)
 end
 
--- @readonly
--- @private
-function Camera:FromBuffer(object: Instance)
-	if posbuffer[object] then
-		return posbuffer[object]
-	end
+-- Calculate the new position of the object based on its original position and the camera's movement
+function Camera.Render() 
+    for _, objectData: ObjectData in ipairs(Camera.Objects) do
+        -- Calculate the movement of the camera since the last update
+        local cameraMovement = Camera.Position - prePosition
+
+        -- Calculate the new position of the object by adding the camera movement to its original position
+        local newPosition = objectData.OriginalPosition + UDim2.fromOffset(cameraMovement.X, cameraMovement.Y)
+
+        if objectData.IsRigidbody then
+            local vertexForces = {}
+
+            for _, vertex in ipairs(objectData.Object:GetVertices()) do
+                vertexForces[vertex.id] = vertex.forces
+            end
+
+            objectData.Object:SetPosition(newPosition.X.Offset, newPosition.Y.Offset)
+
+            for _, vertex in ipairs(objectData.Object:GetVertices()) do
+                vertex:ApplyForce(vertexForces[vertex.id])
+            end
+
+            objectData.OriginalPosition = newPosition
+
+            continue
+        end
+
+        -- Use the ChangePosition method to directly set the new position for the object
+        objectData.Object.Position = newPosition
+        objectData.OriginalPosition = newPosition
+    end
+
+    prePosition = Camera.Position
 end
 
-function Camera:Attach(object: Instance | { any }) --> for now to make the :Attach() work call it twice on the objet
-	Util.Assert(Strings.ExpectedNoArg, object, "Instance", "table")
+function Camera.RenderRigidbodies(rigidbodies: {any})
+    for _, rigidbody: any in ipairs(rigidbodies) do
+         -- Calculate the movement of the camera since the last update
+         local cameraMovement = Camera.Position - prePosition
+         
+         local object = rigidbody:GetFrame()
 
-	-- check if its not attached to the camera already
-	local isFound, _ = Util.FindInTable(self.Objects, object, "Object")
-	if isFound then
-		return warn(string.format(Strings.ObjectAlreadyAttached, object.Name or object.id or "Unknown"))
-	end
+         -- Calculate the new position of the object by adding the camera movement to its original position
+         local newPosition = object.Position + UDim2.fromOffset(cameraMovement.X, cameraMovement.Y)
 
-	--[[ 
-	-- listen to UpdateFinished to update the rigidbody's position
-	if object.id then
-		local initPos = object.Position 
-		object.UpdateFinished:Connect(function()
-			local X = -self.Position.X - initPos.X.Offset
-			local Y = -self.Position.Y - initPos.Y.Offset
-
-			object:SetPosition(X, Y)
-		end)
-	end *]]
-
-	local initialPosition = type(object) == "table" and object:GetFrame().Position or object.Position
-	posbuffer[type(object) == "table" and object:GetFrame() or object] = Vector2.new(initialPosition)
-
-	table.insert(self.Objects, {
-		Object = object,
-		Position = initialPosition,
-	})
+         rigidbody:SetPosition(newPosition.X.Offset, newPosition.Y.Offset)
+    end
 end
 
-function Camera:Detach(object: Instance | { any })
-	Util.Assert(Strings.ExpectedNoArg, object, "Instance", "table")
+function Camera.Detach() end
 
-	-- check if its attached to the camera already
-	local isFound, index = Util.FindInTable(self.Objects, object, "Object")
-	if not isFound then
-		return warn(string.format(Strings.ObjectIsNotAttached, object.Name or object.id or "Unknown"))
-	end
+function Camera.MoveTo(x: number, y: number) 
+    -- Update the prePosition variable with the current position of the camera
+    prePosition = Camera.Position
+    Camera.Position = Vector2.new(x, y)
 
-	table.remove(self.Objects, index)
+    warn(x, y)
 end
 
-function Camera:MoveTo(newPosition: UDim2 | Vector2, offset: Vector2)
-	Util.Assert(Strings.ExpectedNoArg, newPosition, "UDim2", "Vector2")
-
-	self.Position = typeof(offset) == "Vector2" and Util.ConvertToVector(newPosition) + offset
-		or Util.ConvertToVector(newPosition)
-end
-
---[[ function Camera:ScaleToViewport()
-	local scaleToFit = self.GameFrame.AbsoluteSize / Vector2.new(280 * 16 / 9, 280)
-	local scale = math.ceil(math.max(scaleToFit.X, scaleToFit.Y))
-	--local scale = math.max(scaleToFit.X, scaleToFit.Y)
-	viewScale.Scale = scale
-end ]]
-
-function Camera:Zoom(amount: number, tweenInfo: TweenProperty?)
-	if typeof(tweenInfo) == "table" then
-		if zoomTween then
-			zoomTween:Cancel()
-			zoomTween = nil
-		end
-
-		zoomTween = TweenService:Create(
-			viewScale,
-			TweenInfo.new(
-				tweenInfo.Duration or 1,
-				tweenInfo.Style or Enum.EasingStyle.Linear,
-				tweenInfo.Direction or Enum.EasingDirection.Out
-			),
-			{ Scale = amount }
-		)
-		zoomTween:Play()
-	else
-		viewScale.Scale = amount
-	end
-end
+function Camera.Zoom() end
 
 return Camera
