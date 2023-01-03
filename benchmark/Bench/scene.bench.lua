52c919e85d3642a11bf76392e9b34115add2c214
James <86011536+jammees@users.noreply.github.com>
2023-01-03 21:08:08 +0100

    Updated scene benchmark

--- benchmark/Bench/scene.bench.lua
+++ benchmark/Bench/scene.bench.lua
@@ -1,4 +1,4 @@
-local Rethink = require(script.Parent.Parent.Parent.RethinkEngine)
+local Rethink = require(script.Parent.Parent.Parent.Rethink)
 local Scene = Rethink.Scene
 
 local sceneName = "Stess_test"
@@ -6,8 +6,7 @@ local sceneName = "Stess_test"
 return function(Benchmark)
 	Benchmark({
 			["load in a scene"] = function()
-				local sceneData =
-					require(game.Players.LocalPlayer.PlayerScripts["Manual tests"].Controllers.Scene.Scenes[sceneName])
+				local sceneData = require(game.Players.LocalPlayer.PlayerScripts["Manual tests"].Scenes[sceneName])
 				Scene.Load(sceneData[1], sceneData[2])
 			end,
 		})
@@ -17,10 +16,6 @@ return function(Benchmark)
 		:BeforeEach({
 			["load in a scene"] = function()
 				Scene.Flush()
-				print(
-					#Scene:GetObjects() > 0 and #Scene:GetObjects() .. " objects left in memory after flush!"
-						or "No objects left in memory after flush!"
-				)
 			end,
 		})
 		:Run(50)
