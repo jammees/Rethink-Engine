eae28ecd9b07b81d7dbb642fd02e5f396a8de50d
James <86011536+jammees@users.noreply.github.com>
2023-01-03 21:51:39 +0100

    Updated strings

--- src/Components/Debug/Strings.lua
+++ src/Components/Debug/Strings.lua
@@ -8,17 +8,32 @@ return {
     |_| \_\___|\__|_| |_|_|_| |_|_|\_\  |_____|_| |_|\__, |_|_| |_|\___|
                                                      |___/              
     Versatile, easy-to-use 2D game engine.
-    Version: 0.6.0 - DEV
-
+    Version: 1.0.0-alpha
     ]],
 
-	-- Console templates
 	Expected = "Expected to get %q; got %q (arg num %d)!",
 	ExpectedNoArg = "Expected to get %q; got %q!",
-	ObjectAlreadyAttached = "Object (%s) is already attached to camera!",
-	ObjectIsNotAttached = "Object (%s) is not attached to camera!",
+	ExpectedVariable = "Expected %s to be a %s; got %q!",
+
+	ObjectAlreadyAttached = "%s (%s) is already attached to %s!",
+	ObjectIsNotAttached = "%s (%s) is not attached to %s!",
 	RemoveErrorNoObject = "%s (%s) doesn't exist in scene!",
 	EmptySceneTable = "Received an invalid SceneTable; has no items!",
 	SignalNotFound = "%s is not valid signal!",
 	MethodFailNoScene = "Attempted to %s the scene, but failed due to it being empty!",
+	IsWrongType = "Error whilst trying to call %q, expected to get %q; got %q!",
+
+	-- Pretty sure there was a much better way of doing this, but since
+	-- I already took my time writing these down, I decided not to worry
+	Animator = {
+		NoAnimation = "Error whilst trying to call :Play(), no animation was specified!",
+		NoObjectsAttached = "Error whilst trying to call :Play(), no objects were attached!",
+		NotValidObject = "Error whilst trying to call :AttachObject(), expected an ImageLabel or ImageButton got; %q!",
+		AnimationRunning = "Error whilst trying to call %s, Animator is running! Please use :Stop() before this method!",
+		ObjectAlreadyAttached = "Error whilst trying to call :AttachObject(), object already attached!",
+	},
+
+	TaskDistributor = {
+		AlreadyProcessing = "Error whilst trying to call :Distribute(), already processing a chunk!"
+	}
 }
