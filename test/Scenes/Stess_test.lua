c5f69b485b92507a960f69d53d931a0540aeb6bc
James <86011536+jammees@users.noreply.github.com>
2023-01-07 14:33:26 +0100

    Improve stress test
    Cleaned it up, and made it more complex for the compiler

--- test/Scenes/Stess_test.lua
+++ test/Scenes/Stess_test.lua
@@ -1,16 +1,14 @@
-local AMOUNT_OF_OBJCECTS = 2000
+local OBJECT_NUMBER = 2000
 
 local Rethink = require(game:GetService("ReplicatedStorage").Rethink)
 
-local Type = Rethink.Scene.Symbols.Type
-local Property = Rethink.Scene.Symbols.Property
-local Tag = Rethink.Scene.Symbols.Tag
+local Symbols = Rethink.Scene.Symbols
 
 local function createObjects(amount: number)
 	local groupTable = {
-		[Property] = {
+		[Symbols.Property] = {
 			Transparency = 0.5,
-			[Tag] = "Interactibles",
+			[Symbols.Tag] = "Interactibles",
 		},
 	}
 
@@ -21,7 +19,8 @@ local function createObjects(amount: number)
 			Size = UDim2.fromOffset(100, 100),
 			Image = "rbxassetid://30115084",
 
-			[Tag] = "Box",
+			[Symbols.Tag] = "Box",
+			[Symbols.Event("MouseMoved")] = function() end,
 		})
 	end
 
@@ -32,27 +31,38 @@ return {
 	{ Name = "Stress test" },
 	{
 		Interactibles = {
-			[Type] = "UiBase",
-			[Property] = {
+			[Symbols.Type] = "UiBase",
+			[Symbols.Property] = {
 				Class = "ImageButton",
 				BackgroundColor3 = Color3.fromRGB(255, 125, 25),
 
-				[Tag] = "UniversalTag",
+				[Symbols.Tag] = "UniversalTag",
 			},
 
-			createObjects(AMOUNT_OF_OBJCECTS / 2),
+			createObjects(OBJECT_NUMBER / 2),
 		},
 
 		Rigidbodies = {
-			[Type] = "Rigidbody",
-			[Property] = {
+			[Symbols.Type] = "Rigidbody",
+			[Symbols.Property] = {
 				Class = "ImageButton",
 				BackgroundColor3 = Color3.fromRGB(25, 190, 255),
 
-				[Tag] = "UniversalTag",
+				[Symbols.Tag] = "UniversalTag",
+				[Symbols.Rigidbody] = {
+					Collidable = true,
+					Anchored = false,
+					KeepInCanvas = false,
+					CanRotate = false,
+					LifeSpan = 5,
+					Gravity = Vector2.new(1, 1),
+					Friction = 1,
+					AirFriction = 1,
+					Mass = 5,
+				},
 			},
 
-			createObjects(AMOUNT_OF_OBJCECTS / 2),
+			createObjects(OBJECT_NUMBER / 2),
 		},
 	},
 }
