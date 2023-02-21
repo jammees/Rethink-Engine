ac02be97c3249ab5a9d3a7ce72d27f924c52e490
James <86011536+jammees@users.noreply.github.com>
2023-02-21 23:48:49 +0100

    Documentation and edge case fixed

--- src/Tools/Core/Scene/Symbols.lua
+++ src/Tools/Core/Scene/Symbols.lua
@@ -1,9 +1,12 @@
+type Rigidbody = { [string]: any }
+
 type AvailableSymbols = {
 	Property: any,
 	Type: any,
 	Tag: any,
 	Rigidbody: any,
-	Event: any,
+	Event: (propertyName: string) -> Instance | Rigidbody,
+	ShouldFlush: any,
 }
 
 -- 0 : default
@@ -14,8 +17,10 @@ local AVAILABLE_SYMBOLS = {
 	Tag = 0,
 	Rigidbody = 0,
 	Event = 1,
+	ShouldFlush = 0,
 }
 
+local HttpService = game:GetService("HttpService")
 local CollectionService = game:GetService("CollectionService")
 
 local Rigidbody = require(script.Parent.Parent.Parent.Environment.Physics.Physics.RigidBody)
@@ -35,7 +40,7 @@ local function CreateSymbol(symbolName: string, symbolData: any?): Types.Symbol
 		},
 
 		-- This exists only to make each symbol unique
-		__identifier = game:GetService("HttpService"):GenerateGUID(false),
+		__identifier = HttpService:GenerateGUID(false),
 	}
 end
 
@@ -77,6 +82,20 @@ Symbols.Types = TypesHandle
 	Such as Tags, Events.
 ]]
 Symbols.AttachableSymbols = {
+	--[=[
+		Event
+
+		A symbol used to run code when the specified event gets fired.
+
+		```lua
+		-- Scene data
+		MyObject = {
+			[Symbols.Event("MouseButton1Click")] = function()
+				print("MyObject clicked!")
+			end
+		}
+		```
+	]=]
 	Event = function(object: Types.SceneObject, symbol: Types.Symbol)
 		local visualObject = IsRigidbody(object.Object) and object.Object:GetFrame() or object.Object
 
@@ -85,6 +104,29 @@ Symbols.AttachableSymbols = {
 		end))
 	end,
 
+	--[=[
+		Tag
+
+		A symbol used to attach a tag to the given UI element, that can
+		be collected with `CollectionService`.
+
+		For rigidbodies `Scene.GetBodyFromTag` is recommended.
+
+		```lua
+		-- Scene data
+		MyObject = {
+			[Symbols.Tag] = "Hello world!"
+		}
+
+		-- If it's not a rigidbody:
+		-- This will print the first object that is tagged with Hello world!
+		game:GetService("CollectionService"):GetTagged("Hello world!")[1]
+
+		-- If it's a rigidbody:
+		-- This will print all of the rigidbodies that are tagged with `Hello world!`.
+		print(Rethink.Scene.GetBodyFromTag("Hello world!"))
+		```
+	]=]
 	Tag = function(object: Types.SceneObject, symbol: Types.Symbol)
 		local visualObject = IsRigidbody(object.Object) and object.Object:GetFrame() or object.Object
 
@@ -97,8 +139,23 @@ Symbols.AttachableSymbols = {
 		end
 	end,
 
-	-- For some reason the last Property entry gets processed.
-	-- Most likely I won't fix this behaviour, because why would you do such thing? (Hopefully not because to try to break everything :) )
+	--[=[
+		Property
+
+		A symbol used to apply properties to an object.
+
+		```lua
+		-- Scene data
+		MyObject = {
+			[Symbols.Property] = {
+				BackgroundColor3 =  Color3.fromRGB(36, 175, 180)
+			}
+		}
+		```
+
+		In this example, MyObject's background will become blue instead
+		of the default white.
+	]=]
 	Property = function(object: Types.SceneObject, symbol: Types.Symbol)
 		local visualObject = IsRigidbody(object.Object) and object.Object:GetFrame() or object.Object
 
@@ -110,9 +167,36 @@ Symbols.AttachableSymbols = {
 			visualObject[propertyName] = propertyValue
 		end
 	end,
+
+	--[=[
+		ShouldFlush
+
+		A symbol used to prevent `Scene` from deleting the object(s).
+		
+		```lua
+		-- Scene data
+		MyObject = {
+			[Symbols.ShouldFlush] = false
+		}
+		```
+		
+		However if it is required to clear out everything, then when calling `Scene.Flush()`
+		include a `true` argument:
+
+		```lua
+		-- This will result in Scene ignoring the ShouldFlush symbol and it's
+		-- value
+		Scene.Flush(true)
+		```
+	]=]
+	ShouldFlush = function(object: Types.SceneObject, symbol: Types.Symbol)
+		object.ShouldFlush = symbol.SymbolData.Attached
+	end,
 }
 
 -- If callback is not provided, it will return the symbol and it's value
+-- Does not account for multiple symbols being the same type in the same table.
+-- Won't fix most likely, since why would you do that?
 function Symbols.FindSymbol(array: { any }, targetSymbol: string)
 	if typeof(array) ~= "table" then
 		return
@@ -134,6 +218,15 @@ function Symbols.AttachToInstance(object: Types.SceneObject, symbols: { [string]
 	-- because it already loops over this table and checks for each of the symbols
 	for symbolName: string, collectedSymbols: { [number]: Types.Symbol } in pairs(symbols) do
 		for _, symbol: Types.Symbol in ipairs(collectedSymbols) do
+			local symbolProcessor = Symbols.AttachableSymbols[symbolName]
+
+			-- If it is not an attachable symbol just skip it and move to the next one
+			-- no need to notify the user of it being skipped, because it is very less likely
+			-- for someone to use a custom symbol outside of the `Symbols` module
+			if symbolProcessor == nil then
+				continue
+			end
+
 			Symbols.AttachableSymbols[symbolName](object, symbol)
 		end
 	end
