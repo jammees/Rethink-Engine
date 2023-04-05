48c1a66040c7dc2f7754304378994cffd7c2c954
James <86011536+jammees@users.noreply.github.com>
2023-04-06 00:25:41 +0200

    Attempt to make it work with rigidbodies
    Spoiler: I failed

--- src/Tools/Core/Camera.lua
+++ src/Tools/Core/Camera.lua
@@ -11,8 +11,6 @@ type ObjectData = {
 	OriginalPosition: UDim2,
 }
 
-local RunService = game:GetService("RunService")
-
 local Types = require(script.Parent.Scene.Types)
 local Scene = require(script.Parent.Scene)
 
@@ -25,63 +23,56 @@ Camera.Position = Vector2.new(0, 0)
 Camera.Objects = {}
 Camera.ShouldRender = true
 
--- Calculate the new position of the object based on its original position and the camera's movement
--- TODO: Cleanup
--- TODO: Create different type of modes:
--- Such as: Top-Down, Editor-1 (no-zoom), Editor-2 (with zoom), Side View
 function Camera.Render(deltaTime: number)
 	if Camera.ShouldRender == false or prePosition == Camera.Position then
 		return
 	end
 
 	for _, objectData: ObjectData in ipairs(Camera.Objects) do
-		-- Calculate the movement of the camera since the last update
-		local cameraMovement = Camera.Position - prePosition
-
-		if objectData.IsRigidbody then
-			local newPos = objectData.Object.center + cameraMovement
-
-			if objectData.Object.anchored then
-				objectData.Object:SetPosition(newPos.X, newPos.Y)
-				objectData.OriginalPosition = newPos
-
-				continue
-			end
-
-			local vertexCache = {}
-
-			for _, vertex in ipairs(objectData.Object:GetVertices()) do
-				--vertexCache[vertex.id] = vertex.forces
-				vertexCache[vertex.id] = vertex:Velocity()
-				--vertexCache[vertex.id] = vertex:Velocity2()
-				--vertexCache[vertex.id] = vertex:GetNetForce()
-			end
-
-			-- Create a function to store the position that Camera calculated
-			-- and next time when Nature2D is updating the rigidbodies it should take that pose into account (starting point)
-
-			--objectData.Object:SetPosition(newPosition.X.Offset, newPosition.Y.Offset)
-			objectData.Object:SetPosition(newPos.X, newPos.Y)
-			--objectData.Object:ApplyForce(cameraMovement.X, cameraMovement.Y)
-
-			for _, vertex in ipairs(objectData.Object:GetVertices()) do
-				vertex:ApplyForce(vertexCache[vertex.id])
-			end
-
-			objectData.OriginalPosition = newPos
-
-			continue
-		end
-
-		-- Calculate the new position of the object by adding the camera movement to its original position
-		local newPosition = objectData.OriginalPosition + UDim2.fromOffset(cameraMovement.X, cameraMovement.Y)
-
-		-- Use the ChangePosition method to directly set the new position for the object
-		objectData.OriginalPosition = newPosition
-		objectData.Object.Position = newPosition
+		Camera.RenderIndividual(objectData)
 	end
 
 	prePosition = Camera.Position
+end
+
+function Camera.RenderIndividual(objectData, deltaTime: number)
+	-- Calculate the movement of the camera since the last update
+	local cameraDelta = Camera.Position - prePosition
+
+	if objectData.IsRigidbody then
+		local newPosition = objectData.OriginalPosition + UDim2.fromOffset(cameraDelta.X, cameraDelta.Y)
+
+		if objectData.Object.anchored then
+			objectData.Object:SetPosition(newPosition.X.Offset, newPosition.Y.Offset)
+
+			objectData.OriginalPosition = newPosition
+
+			return
+		end
+
+		local vertexCache = {}
+
+		for _, vertex in ipairs(objectData.Object:GetVertices()) do
+			vertexCache[vertex.id] = vertex:Velocity()
+		end
+
+		objectData.Object:SetPosition(newPosition.X.Offset, newPosition.Y.Offset)
+
+		for _, vertex in ipairs(objectData.Object:GetVertices()) do
+			vertex:ApplyForce(vertexCache[vertex.id])
+		end
+
+		objectData.OriginalPosition = newPosition
+
+		return
+	end
+
+	-- Calculate the new position of the object by adding the camera movement to its original position
+	local newPosition = objectData.OriginalPosition + UDim2.fromOffset(cameraDelta.X, cameraDelta.Y)
+
+	-- Use the ChangePosition method to directly set the new position for the object
+	objectData.Object.Position = newPosition
+	objectData.OriginalPosition = newPosition
 end
 
 -- Store the original position of the object when it is attached to the camera
@@ -97,19 +88,19 @@ function Camera.Attach(object: GuiBase2d | Types.Rigidbody)
 		OriginalPosition = isRigidbody and object:GetFrame().Position or object.Position,
 	}
 
-	table.insert(Camera.Objects, objectReference)
-
 	-- Apply position
-	local cameraMovement = Camera.Position - prePosition
+	--[[ local cameraDelta = Camera.Position - prePosition
 	if Scene.IsRigidbody(object) then
-		local newPos = object.center + cameraMovement
-		object:SetPosition(newPos.X, newPos.Y)
-		objectReference.OriginalPosition = newPos
-	else
-		local newPosition = objectReference.OriginalPosition + UDim2.fromOffset(cameraMovement.X, cameraMovement.Y)
+		local newPosition = objectReference.OriginalPosition + UDim2.fromOffset(cameraDelta.X, cameraDelta.Y)
+		object:SetPosition(newPosition.X.Offset, newPosition.Y.Offset)
 		objectReference.OriginalPosition = newPosition
-		objectReference.Object.Position = newPosition
-	end
+	else
+		local newPosition = objectReference.OriginalPosition + UDim2.fromOffset(cameraDelta.X, cameraDelta.Y)
+		object.Position = newPosition
+		objectReference.OriginalPosition = newPosition
+	end ]]
+
+	table.insert(Camera.Objects, objectReference)
 end
 
 function Camera.Detach(object: GuiBase2d | { any })
@@ -121,16 +112,10 @@ function Camera.Detach(object: GuiBase2d | { any })
 end
 
 function Camera.MoveTo(x: number, y: number)
-	if RunService:IsStudio() then
-		warn("[Rethink] Camera is still in a prototyping phase! API is highly unstable and likely to cause problems!")
-	end
-
 	-- Update the prePosition variable with the current position of the camera
 	prePosition = Camera.Position
 	Camera.Position = Vector2.new(x, y)
 end
-
-function Camera.Zoom() end
 
 -- Automatically call :Attach on every object that gets loaded
 -- with scene.
@@ -139,10 +124,8 @@ Scene.Events.ObjectAdded:Connect(function(object)
 	Camera.Attach(object)
 end)
 
-Scene.Events.FlushStarted:Connect(function()
-	for _, object in ipairs(Camera.Objects) do
-		Camera.Detach(object)
-	end
+Scene.Events.ObjectRemoved:Connect(function(object)
+	Camera.Detach(object)
 end)
 
 return Camera
