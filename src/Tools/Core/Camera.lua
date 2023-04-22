755c0ef16e7cddb44667afac32a71ef2b1e390c7
James <86011536+jammees@users.noreply.github.com>
2023-04-22 14:19:55 +0200

    Fixed Camera so that is usable

--- src/Tools/Core/Camera.lua
+++ src/Tools/Core/Camera.lua
@@ -11,121 +11,191 @@ type ObjectData = {
 	OriginalPosition: UDim2,
 }
 
-local Types = require(script.Parent.Scene.Types)
 local Scene = require(script.Parent.Scene)
+local Template = require(script.Parent.Parent.Utility.Template)
+local Physics = Template.FetchGlobal("__Rethink_Physics")
 
+local runnerConnection = nil
 local prePosition = Vector2.new(0, 0)
-local objectLookup = {}
 
-local Camera = {}
-
-Camera.Position = Vector2.new(0, 0)
-Camera.Objects = {}
-Camera.ShouldRender = true
-
-function Camera.Render(deltaTime: number)
-	if Camera.ShouldRender == false or prePosition == Camera.Position then
-		return
-	end
-
-	for _, objectData: ObjectData in ipairs(Camera.Objects) do
-		Camera.RenderIndividual(objectData)
-	end
-
-	prePosition = Camera.Position
-end
-
-function Camera.RenderIndividual(objectData, deltaTime: number)
-	-- Calculate the movement of the camera since the last update
-	local cameraDelta = Camera.Position - prePosition
-
-	if objectData.IsRigidbody then
-		local newPosition = objectData.OriginalPosition + UDim2.fromOffset(cameraDelta.X, cameraDelta.Y)
-
-		if objectData.Object.anchored then
-			objectData.Object:SetPosition(newPosition.X.Offset, newPosition.Y.Offset)
-
-			objectData.OriginalPosition = newPosition
-
-			return
-		end
-
-		local vertexCache = {}
-
-		for _, vertex in ipairs(objectData.Object:GetVertices()) do
-			vertexCache[vertex.id] = vertex:Velocity()
-		end
-
-		objectData.Object:SetPosition(newPosition.X.Offset, newPosition.Y.Offset)
-
-		for _, vertex in ipairs(objectData.Object:GetVertices()) do
-			vertex:ApplyForce(vertexCache[vertex.id])
-		end
-
-		objectData.OriginalPosition = newPosition
-
-		return
-	end
-
-	-- Calculate the new position of the object by adding the camera movement to its original position
-	local newPosition = objectData.OriginalPosition + UDim2.fromOffset(cameraDelta.X, cameraDelta.Y)
-
-	-- Use the ChangePosition method to directly set the new position for the object
-	objectData.Object.Position = newPosition
-	objectData.OriginalPosition = newPosition
-end
-
--- Store the original position of the object when it is attached to the camera
-function Camera.Attach(object: GuiBase2d | Types.Rigidbody)
+local function GetState<OBJ>(object: OBJ)
 	local isRigidbody = Scene.IsRigidbody(object)
-
-	local reservedPosition = #Camera.Objects + 1
-	objectLookup[object] = reservedPosition
-
-	local objectReference = {
-		Object = object,
-		IsRigidbody = isRigidbody,
-		OriginalPosition = isRigidbody and object:GetFrame().Position or object.Position,
-	}
-
-	-- Apply position
-	--[[ local cameraDelta = Camera.Position - prePosition
-	if Scene.IsRigidbody(object) then
-		local newPosition = objectReference.OriginalPosition + UDim2.fromOffset(cameraDelta.X, cameraDelta.Y)
-		object:SetPosition(newPosition.X.Offset, newPosition.Y.Offset)
-		objectReference.OriginalPosition = newPosition
-	else
-		local newPosition = objectReference.OriginalPosition + UDim2.fromOffset(cameraDelta.X, cameraDelta.Y)
-		object.Position = newPosition
-		objectReference.OriginalPosition = newPosition
-	end ]]
-
-	table.insert(Camera.Objects, objectReference)
+	return isRigidbody, isRigidbody and object.anchored
 end
 
-function Camera.Detach(object: GuiBase2d | { any })
-	local dataIndex = objectLookup[object]
+local Camera = {
+	Position = Vector2.new(0, 0),
+	Objects = {},
+	IsRunning = false,
+	XBounds = NumberRange.new(-math.huge, math.huge),
+	YBounds = NumberRange.new(-math.huge, math.huge),
+}
 
-	if dataIndex then
-		table.remove(Camera.Objects, dataIndex)
+-- Define Render Handlers that sole purpose is to update the objects
+-- positions based on the Camera
+Camera.Handlers = {}
+Camera.Handlers.NonBody = function(objectData: any, cameraDelta: Vector2, deltaTime: number?)
+	local object = objectData.Object
+	local origin: UDim2 = objectData.Origin
+
+	origin += UDim2.fromOffset(cameraDelta.X, cameraDelta.Y)
+
+	if deltaTime then
+		origin += UDim2.fromOffset(cameraDelta.X * deltaTime, cameraDelta.Y * deltaTime)
+	end
+
+	object.Position = origin
+	objectData.Origin = origin
+end
+Camera.Handlers.AnchoredBody = function(objectData: any, cameraDelta: Vector2, deltaTime: number?)
+	local body = objectData.Object
+	local origin: UDim2 = objectData.Origin
+
+	origin += UDim2.fromOffset(cameraDelta.X, cameraDelta.Y)
+
+	if deltaTime then
+		origin += UDim2.fromOffset(cameraDelta.X * deltaTime, cameraDelta.Y * deltaTime)
+	end
+
+	-- Convert UDim2 to Vector2
+	local viewportSize = workspace.CurrentCamera.ViewportSize
+	local scaleX = origin.X.Scale * viewportSize.X
+	local scaleY = origin.Y.Scale * viewportSize.Y
+
+	local convertedOrigin = Vector2.new(scaleX + origin.X.Offset, scaleY + origin.Y.Offset)
+
+	body.anchorPos = convertedOrigin
+
+	objectData.Origin = origin
+end
+Camera.Handlers.NonAnchoredBody = function(objectData: any, cameraDelta: Vector2, deltaTime: number?)
+	local body = objectData.Object
+	--local origin: UDim2 = objectData.Origin
+	local origin = body.center
+
+	origin += Vector2.new(cameraDelta.X, cameraDelta.Y)
+
+	if deltaTime then
+		origin += Vector2.new(cameraDelta.X * deltaTime, cameraDelta.Y * deltaTime)
+	end
+
+	local forces = {}
+
+	for _, vertex in body:GetVertices() do
+		forces[vertex.id] = vertex.forces
+	end
+
+	body:SetPosition(origin.X, origin.Y)
+
+	for _, vertex in body:GetVertices() do
+		vertex.forces = forces[vertex.id]
+	end
+
+	objectData.Origin = origin
+end
+
+function Camera.GetHandler(objectData)
+	local isRigidbody, isAnchored = GetState(objectData.Object)
+
+	if not isRigidbody then
+		return Camera.Handlers.NonBody
+	elseif isRigidbody and isAnchored then
+		return Camera.Handlers.AnchoredBody
+	elseif isRigidbody and not isAnchored then
+		return Camera.Handlers.NonAnchoredBody
 	end
 end
 
-function Camera.MoveTo(x: number, y: number)
-	-- Update the prePosition variable with the current position of the camera
+-- If deltaTime is provided it will multiply the final position by deltaTime
+-- if not it won't
+function Camera.Render(deltaTime: number?)
+	if Camera.Position == prePosition then
+		return
+	end
+
+	local delta = Camera.Position - prePosition
+
+	-- Loop trough objects
+	for _, objectData in Camera.Objects do
+		local handler = Camera.GetHandler(objectData)
+
+		-- Pass the data into the handler
+		handler(objectData, delta, deltaTime)
+	end
+
+	-- Set the prePosition to the new position to make .Render
+	-- stop updating if it's the same + more accurate delta
 	prePosition = Camera.Position
-	Camera.Position = Vector2.new(x, y)
 end
 
--- Automatically call :Attach on every object that gets loaded
--- with scene.
--- In the future, this might get a setting to turn this behaviour off
-Scene.Events.ObjectAdded:Connect(function(object)
-	Camera.Attach(object)
-end)
+function Camera.Attach(object)
+	local originPosition = nil
 
-Scene.Events.ObjectRemoved:Connect(function(object)
-	Camera.Detach(object)
-end)
+	local isRigidbody, isAnchored = GetState(object)
+
+	if not isRigidbody then
+		originPosition = object.Position
+	elseif isRigidbody and isAnchored then
+		originPosition = object:GetFrame().Position
+	elseif isRigidbody and not isAnchored then
+		originPosition = object.center
+	end
+
+	local objectData = { Object = object, Origin = originPosition }
+
+	-- Apply transformation
+	local handler = Camera.GetHandler(objectData)
+	if handler then
+		handler(objectData, Camera.Position)
+	else
+		return warn(`Unexpected object: No Render Handler was found -> Object was not attached!`)
+	end
+
+	table.insert(Camera.Objects, objectData)
+end
+
+function Camera.Detach(object)
+	for index, obj in Camera.Objects do
+		if obj.Object == object then
+			table.remove(Camera.Objects, index)
+		end
+	end
+end
+
+function Camera.SetPosition(x: number, y: number)
+	-- Clamp the new x and y to the given boundary
+	Camera.Position = Vector2.new(
+		math.clamp(x, Camera.XBounds.Min, Camera.XBounds.Max),
+		math.clamp(y, Camera.YBounds.Min, Camera.YBounds.Max)
+	)
+end
+
+function Camera.Start()
+	if Camera.IsRunning then
+		return warn("Camera is already running!")
+	end
+
+	runnerConnection = Physics.Updated:Connect(function(deltaTime)
+		Camera.Render(deltaTime)
+	end)
+
+	Camera.IsRunning = true
+end
+
+function Camera.Stop()
+	if not Camera.IsRunning then
+		return warn("Camera is not running!")
+	end
+
+	runnerConnection:Disconnect()
+	runnerConnection = nil
+
+	Camera.IsRunning = false
+end
+
+function Camera.SetBoundary(XBounds: NumberRange, YBounds: NumberRange)
+	Camera.XBounds = XBounds
+	Camera.YBounds = YBounds
+end
 
 return Camera
