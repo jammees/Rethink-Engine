7abf1bcef8d1ab9f504a629b7c7e5e5526d60e72
James <86011536+jammees@users.noreply.github.com>
2023-07-27 17:02:06 +0200

    Describe `CompilerChunkSize` setting

--- src/Settings.lua
+++ src/Settings.lua
@@ -46,7 +46,7 @@ return {
 		ExtensionSize = 10, -- How many objects should be created to refill the pool if there are no available ones
 	},
 
-	CompilerChunkSize = 100,
+	CompilerChunkSize = 100, -- The lower the number the faster the loading process will take, however with more lag
 	ViewportColor = Color3.fromRGB(35, 68, 139),
 
 	-- Here are all of the settings related to the physics engine
