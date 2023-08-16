a06a66251242e1134129ae14649b556f38b2d135
James <86011536+jammees@users.noreply.github.com>
2023-08-17 00:08:39 +0200

    Update test scene to reflect symbol changes

--- test/Scenes/Test.scene.lua
+++ test/Scenes/Test.scene.lua
@@ -7,14 +7,13 @@ return {
 	Name = "Test",
 
 	Interactibles = {
-		[Symbols.Type] = "UiBase",
+		[Symbols.Type] = "UIBase",
 		[Symbols.Property] = {
-			Class = "ImageButton",
 			BackgroundColor3 = Color3.fromRGB(255, 125, 25),
 			Transparency = 0.5,
 			[Symbols.Tag] = "Interactibles",
-
 			[Symbols.Tag] = "UniversalTag",
+			[Symbols.Class] = "ImageButton",
 		},
 
 		Box = {
@@ -24,26 +23,27 @@ return {
 			Image = "rbxassetid://30115084",
 
 			[Symbols.Tag] = "Box",
-			[Symbols.Event("MouseButton1Click")] = function(object: ImageButton)
-				print("Box has been clicked!")
-				object.ImageColor3 = Color3.fromRGB(math.random(0, 255), math.random(0, 255), math.random(0, 255))
-			end,
-			[Symbols.ShouldFlush] = false,
+			[Symbols.Permanent] = true,
+			[Symbols.testSymbol] = true,
 
-			[Symbols.Event("MouseButton2Click")] = function(object: ImageButton)
-				Scene.Remove(object)
-				warn(Scene.GetObjects())
+			[Symbols.Event("MouseButton1Click")] = function(thisObject: ImageButton)
+				print("Box has been clicked!")
+				thisObject.ImageColor3 = Color3.fromRGB(math.random(0, 255), math.random(0, 255), math.random(0, 255))
+			end,
+
+			[Symbols.Event("MouseButton2Click")] = function(thisObject: ImageButton)
+				Scene.Cleanup(thisObject)
 			end,
 		},
 
 		HelloWorld = {
-			Class = "ImageLabel",
 			AnchorPoint = Vector2.new(0.4, 0.5),
 			Position = UDim2.fromScale(0.4, 0.5),
 			Size = UDim2.fromOffset(100, 100),
 
+			[Symbols.Class] = "ImageButton",
 			[Symbols.Tag] = "AnimationTag",
-			[Symbols.ShouldFlush] = false,
+			[Symbols.Permanent] = true,
 		},
 
 		Empty = {},
@@ -52,9 +52,9 @@ return {
 	Rigidbodies = {
 		[Symbols.Type] = "Rigidbody",
 		[Symbols.Property] = {
-			Class = "ImageButton",
 			BackgroundColor3 = Color3.fromRGB(50, 236, 33),
 
+			[Symbols.Class] = "ImageButton",
 			[Symbols.Tag] = "UniversalTag",
 			[Symbols.Rigidbody] = {
 				KeepInCanvas = false,
@@ -63,7 +63,6 @@ return {
 		},
 
 		RigidBox = {
-			-- Class = "ImageLabel",
 			AnchorPoint = Vector2.new(0.5, 0.5),
 			Position = UDim2.fromScale(0.8, 0.5),
 			BackgroundColor3 = Color3.fromRGB(16, 161, 245),
@@ -71,22 +70,21 @@ return {
 			Size = UDim2.fromOffset(101, 101),
 
 			[Symbols.Tag] = "MyRigidbody",
-			[Symbols.Event("MouseButton1Click")] = function(self)
-				self:Destroy()
-				--Scene.Remove(self)
+			[Symbols.Event("MouseButton1Click")] = function(thisObject: ImageButton)
+				Scene.Cleanup(thisObject)
 			end,
 		},
 
 		Ground = {
-			Class = "Frame",
 			AnchorPoint = Vector2.new(0.5, 0),
-			Position = UDim2.fromScale(0.5, 1),
+			Position = UDim2.new(0.5, 0, 1, -50),
 			Size = UDim2.fromOffset(1500, 50),
 			BackgroundColor3 = Color3.fromRGB(126, 126, 126),
+
+			[Symbols.Class] = "Frame",
 			[Symbols.Rigidbody] = {
 				Anchored = true,
 			},
-			--[Symbols.ShouldFlush] = false,
 		},
 	},
 }
