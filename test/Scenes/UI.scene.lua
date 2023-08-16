0447053c05393f38723f72cbd07623607adf70de
James <86011536+jammees@users.noreply.github.com>
2023-08-17 00:08:53 +0200

    Add UI scene to test parent and LinkGet

---
+++ test/Scenes/UI.scene.lua
@@ -0,0 +1,53 @@
+---@module src.init
+local Rethink = require(game:GetService("ReplicatedStorage").Rethink)
+local Scene = Rethink.GetModules().Scene
+local Symbols = Scene.Symbols
+
+return {
+	Name = "UI",
+
+	{
+		[Symbols.Type] = "UIBase",
+		[Symbols.Property] = {
+			Parent = Rethink.GetModules().Ui.Ui,
+		},
+
+		Text = {
+			Text = "Hello world!",
+			BackgroundTransparency = 1,
+			TextSize = 21,
+			Font = Enum.Font.GothamMedium,
+			Position = UDim2.fromScale(0.3, 0.5),
+
+			[Symbols.Class] = "TextLabel",
+			[Symbols.LinkTag] = "Text",
+		},
+
+		TextField = {
+			BackgroundTransparency = 1,
+			TextSize = 21,
+			Font = Enum.Font.GothamMedium,
+			Position = UDim2.fromScale(0.3, 0.6),
+			PlaceholderText = "Input text here",
+
+			[Symbols.Class] = "TextBox",
+			[Symbols.LinkTag] = "Field",
+		},
+
+		Button = {
+			BackgroundTransparency = 1,
+			TextSize = 21,
+			Font = Enum.Font.GothamMedium,
+			Position = UDim2.fromScale(0.5, 0.6),
+			Text = "Confirm",
+
+			[Symbols.Class] = "TextButton",
+			[Symbols.LinkGet({ "Field", "Text" })] = function(thisObject: TextButton, field: TextBox, text: TextLabel)
+				local thisRef = Scene.GetObjectReference(thisObject)
+				thisRef.Janitor:Add(thisObject.MouseButton1Click:Connect(function()
+					text.Text = field.Text
+				end))
+			end,
+		},
+	},
+}
