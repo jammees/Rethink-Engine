480c6975bbe7a1d0989162dbafdf1a3b92956694
James <86011536+jammees@users.noreply.github.com>
2023-08-17 00:00:14 +0200

    Change Name and Parent priority
    Use Log, change Promise require path

--- src/Modules/Scene/Objects/UiBase.lua
+++ src/Modules/Scene/Objects/UiBase.lua
@@ -1,13 +1,14 @@
 local Types = require(script.Parent.Parent.Types)
 local Template = require(script.Parent.Parent.Parent.Template)
-local UiPool = Template.FetchGlobal("__Rethink_Pool")
-local Promise = require(script.Parent.Parent.Parent.Parent.Library.Promise)
 
-local objectParent = Template.FetchGlobal("__Rethink_Ui").Viewport
+local Promise = require(script.Parent.Parent.Parent.Parent.Vendors.Promise)
+local Log = require(script.Parent.Parent.Parent.Parent.Library.Log)
 
 function ApplyProperties(object: Instance, properties: { [string]: any })
-	for propertyKey, propertyValue in pairs(properties) do
-		object[propertyKey] = propertyValue
+	for propertyKey, propertyValue in properties do
+		Promise.try(function()
+			object[propertyKey] = propertyValue
+		end):catch(Log.Warn)
 	end
 end
 
@@ -15,19 +16,19 @@ end
     Creates an object by the given arguments, then returns it
 ]]
 local function CompileStatic(data: Types.Prototype_ChunkObject)
+	local UiPool = Template.FetchGlobal("__Rethink_Pool")
+	local objectParent = Template.FetchGlobal("__Rethink_Ui").Viewport
+
 	local object = UiPool:Get(data.ObjectClass)
 
 	Promise.try(function()
-		-- Apply all of the properties at once
-		ApplyProperties(object, data.Properties)
-
-		-- Apply name if not specified
-		object.Name = typeof(data.Properties.Name) and data.Properties.Name or data.ObjectClass
-
 		object.Parent = objectParent
+		object.Name = data.ObjectClass
+
+		ApplyProperties(object, data.Properties)
 	end)
 		:catch(function(errorMessage: string)
-			warn("Encountered an error whilst trying to create an UiBase:\n\n" .. errorMessage)
+			Log.Warn(errorMessage)
 
 			UiPool:Return(object)
 		end)
