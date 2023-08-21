a65ecc77e9e841afa097514fd5d1c99f805a04a5
James <86011536+jammees@users.noreply.github.com>
2023-08-21 21:28:45 +0200

    Update to match new format

--- test/Scenes/ChildrenSymbol.scene.lua
+++ test/Scenes/ChildrenSymbol.scene.lua
@@ -12,12 +12,41 @@ return {
 		Test = {
 			[Symbols.Children] = {
 				a = {
-					Class = "TextButton",
 					BackgroundColor3 = Color3.fromRGB(150, 50, 42),
 
+					[Symbols.Class] = "TextButton",
 					[Symbols.Event("MouseButton1Click")] = function(object)
 						object.BackgroundColor3 = Color3.new(math.random() / 1, math.random() / 1, math.random() / 1)
 					end,
+					[Symbols.Children] = {
+						b = {
+							BackgroundColor3 = Color3.fromRGB(42, 50, 150),
+							Position = UDim2.fromOffset(100, 0),
+
+							[Symbols.Class] = "TextButton",
+							[Symbols.Event("MouseButton2Click")] = function(object)
+								object.BackgroundColor3 =
+									Color3.new(math.random() / 1, math.random() / 1, math.random() / 1)
+							end,
+						},
+					},
+				},
+
+				rigid = {
+					BackgroundColor3 = Color3.fromRGB(255, 255, 0),
+
+					[Symbols.Class] = "ImageLabel",
+					[Symbols.Type] = "Rigidbody",
+					[Symbols.Rigidbody] = {
+						Mass = 5,
+						Collidable = true,
+						-- Anchored = true,
+					},
+					[Symbols.Children] = {
+						corner = {
+							[Symbols.Class] = "UICorner",
+						},
+					},
 				},
 			},
 		},
